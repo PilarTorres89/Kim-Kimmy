5c231b26ba3dcbc0b5d6b3dce0c75767b6442a9b
PilarTorres89 <ptorres@eurekalabs.io>
2024-05-14 14:25:24 -0300

    conf

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    watchForFileChanges: false,
+    setupNodeEvents(on, config) {},
+    baseUrl: 'https://kim-and-kimmy-stg.myshopify.com/'
+    },
+    defaultCommandTimeout: 15000,
+    chromeWebSecurity: false,
+    projectId: 'ds6q9s',
+});
+
