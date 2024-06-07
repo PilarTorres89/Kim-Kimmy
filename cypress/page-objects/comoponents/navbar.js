3c9263d10d0a2b88b741253871409c52ba490085
PilarTorres89 <ptorres@eurekalabs.io>
2024-06-07 13:34:15 -0300

    navbar

--- cypress/page-objects/comoponents/navbar.js
+++ cypress/page-objects/comoponents/navbar.js
@@ -3,7 +3,8 @@ import BasePage from '../BasePage';
 export default class NavBar extends BasePage {
   static menu = 'header > nav > ul';
   static optionMenuCat1 = 'Shop Diapers';
-
+  static optionMenuCat2 = 'Shop Pants';
+  static optionMenuCat3 = 'Shop Wipes';
 
   //static clickMenu() {
   //  cy.get(this.menu).click();
@@ -11,6 +12,14 @@ export default class NavBar extends BasePage {
 
   static clickMenuCategory(cat) {
     cy.get(this.menu).contains(this.optionMenuCat1).click();
+  }
+
+  static clickMenuCategory(cat) {
+    cy.get(this.menu).contains(this.optionMenuCat2).click();
+  }
+  
+  static clickMenuCategory(cat) {
+    cy.get(this.menu).contains(this.optionMenuCat3).click();
   }
 
   static clickMenuSubCategory(subCat) {
