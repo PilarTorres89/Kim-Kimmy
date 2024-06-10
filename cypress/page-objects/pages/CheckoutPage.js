0694f0babae9832f396a812ebea6d849df7d48ca
PilarTorres89 <ptorres@eurekalabs.io>
2024-06-10 08:58:56 -0300

    checkout page

--- cypress/page-objects/pages/CheckoutPage.js
+++ cypress/page-objects/pages/CheckoutPage.js
@@ -1,3 +1,11 @@
 import BasePage from '../BasePage';
 
-export default class CheckoutPage extends BasePage { }
+export default class CheckoutPage extends BasePage { 
+
+static buttoncheckout = "#CartDrawer-Checkout"
+
+
+  static clickbutton() {
+    cy.get(this.buttoncheckout).click()
+  }
+}
